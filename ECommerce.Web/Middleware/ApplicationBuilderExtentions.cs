1fda546690259b0619a14fcf019573a30777c9f1
welat baran <okyanusmavisi1990@hotmail.com>
2022-03-17 20:00:56 +0300

    Add project files.

---
+++ ECommerce.Web/Middleware/ApplicationBuilderExtentions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.FileProviders;
+using System.IO;
+
+namespace ECommerce.Web.Middleware
+{
+    public static class ApplicationBuilderExtentions
+    {
+        public static IApplicationBuilder CustomStaticFiles(this IApplicationBuilder app)
+        {
+            var path = Path.Combine(Directory.GetCurrentDirectory(), "node_modules");
+            var options = new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(path),
+                RequestPath = "/modules"
+            };
+            app.UseStaticFiles(options);
+            return app;
+        }
+    }
+}
